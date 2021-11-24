5a94c41383b26f0c4172706742979e7a143d27b0
Holvert-p <holvertperez@hotmail.com>
2021-11-24 17:01:25 -0600

    m

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let nav_button = document.getElementById('nav-button');
 let nav_menu = document.querySelector('.nav__menu');
+let check = document.getElementById('check');
 
 nav_button.addEventListener('click', () => {
   nav_menu.classList.toggle('nav__menu--show');
@@ -8,3 +9,7 @@ nav_button.addEventListener('click', () => {
 nav_button.addEventListener('blur', () => {
   nav_menu.classList.toggle('nav__menu--show');
 }, true);
+
+check.addEventListener('click', () => {
+  check.checked ? location.href = "../en/index.html" : location.href = "../index.html"
+})
