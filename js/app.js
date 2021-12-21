4e3d25fc3f0a8f6e20677fded597bd495e6d7c78
Holvert-p <holvertperez@hotmail.com>
2021-12-20 20:42:03 -0600

    m

--- js/app.js
+++ js/app.js
@@ -1,43 +1,46 @@
-const nav_button = document.getElementById('nav__button');
-const nav_menu = document.querySelector('.nav__menu');
-const check = document.getElementById('check');
-const check_dark = document.getElementById('check-dark');
-const value = localStorage.getItem('check');
+const nav_button = document.getElementById("nav__button");
+const nav_menu = document.querySelector(".nav__menu");
+const check = document.getElementById("check");
+const check_dark = document.getElementById("check-dark");
+const value = localStorage.getItem("check");
 
-const message__input = document.querySelectorAll('.contact__form [required]');
+const message__input = document.querySelectorAll(".contact__form [required]");
 
-message__input.forEach(el => {
-  const message__error = document.querySelectorAll('.message__error');
+message__input.forEach((el) => {
+  const message__error = document.querySelectorAll(".message__error");
 
-  el.addEventListener('keyup', e => {
-
+  el.addEventListener("keyup", (e) => {
     let $input = e.target,
       pattern = $input.pattern || $input.dataset.pattern;
-    console.log(pattern)
-    console.log($input.value)
+    console.log(pattern);
+    console.log($input.value);
     if (pattern && $input.value !== "") {
       let reg = new RegExp(pattern);
       return !reg.exec($input.value)
-        ? el.nextElementSibling.classList.add('message__error--show')
-        : el.nextElementSibling.classList.remove('message__error--show')
+        ? el.nextElementSibling.classList.add("message__error--show")
+        : el.nextElementSibling.classList.remove("message__error--show");
     }
-
-  })
+  });
 });
-const message__loader = document.querySelector('.message__loader'),
-  message__response = document.querySelector('.message__response'),
-  message__button = document.getElementById('message__button');
+const message__loader = document.querySelector(".message__loader"),
+  message__response = document.querySelector(".message__response"),
+  message__button = document.getElementById("message__button");
 
-document.addEventListener('submit', e => {
+document.addEventListener("submit", (e) => {
   e.preventDefault();
   const elements = e.target.elements;
-  message__button.style.opacity = '0';
-  message__loader.style.opacity = '1';
+  message__button.style.opacity = "0";
+  message__loader.style.opacity = "1";
 
   setTimeout(() => {
-    message__loader.style.opacity = '0';
-    message__response.style.opacity = '1';
-    sendMail(elements['email'].value, elements['message'].value, elements['name'].value, e.target)
+    message__loader.style.opacity = "0";
+    message__response.style.opacity = "1";
+    sendMail(
+      elements["email"].value,
+      elements["message"].value,
+      elements["name"].value,
+      e.target
+    );
   }, 1000);
 });
 
@@ -49,26 +52,40 @@ const sendMail = (email, message, name, form) => {
     To: "pereshol13@gmail.com",
     From: email,
     Subject: "Propuesta de trabajo",
-    Body: "<p><strong>Mensaje:</strong></p><br>" + message + "<br><br><p><strong>Nombre:</strong></p>" + name
+    Body:
+      "<p><strong>Mensaje:</strong></p><br>" +
+      message +
+      "<br><br><p><strong>Nombre:</strong></p>" +
+      name,
   }).then(() => {
-    message__response.style.opacity = '0';
-    message__button.style.opacity = '1';
+    message__response.style.opacity = "0";
+    message__button.style.opacity = "1";
     form.reset();
   });
+};
+if (value === "true") {
+  check_dark.setAttribute("checked", "checked");
 }
-if (value === 'true') {
-  check_dark.setAttribute('checked', 'checked');
-}
-nav_button.addEventListener('click', () => {
-  nav_menu.classList.toggle('nav__menu--show');
+const $nav = document.querySelector(".hamburger");
+nav_button.addEventListener("click", () => {
+  nav_menu.classList.toggle("nav__menu--show");
+
+  $nav.classList.toggle("is-active");
 });
 
-nav_button.addEventListener('blur', () => {
-  nav_menu.classList.remove('nav__menu--show');
-}, true);
+nav_button.addEventListener(
+  "blur",
+  () => {
+    nav_menu.classList.remove("nav__menu--show");
+    $nav.classList.remove("is-active");
+  },
+  true
+);
 
-check.addEventListener('click', () => {
-  check.checked ? (location.href = "../en/", localStorage.setItem('check', check_dark.checked)) : (location.href = "../", localStorage.setItem('check', check_dark.checked))
+check.addEventListener("click", () => {
+  check.checked
+    ? ((location.href = "../en/"),
+      localStorage.setItem("check", check_dark.checked))
+    : ((location.href = "../"),
+      localStorage.setItem("check", check_dark.checked));
 });
-
-
