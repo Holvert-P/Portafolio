41f35bee3c8c7c545adebf611a02093d6b5f4570
Holvert-P <holvertperez@hotmail.com>
2022-01-04 16:12:48 -0600

    m

--- js/app.js
+++ js/app.js
@@ -16,9 +16,9 @@ message__input.forEach((el) => {
     console.log($input.value);
     if (pattern && $input.value !== "") {
       let reg = new RegExp(pattern);
-      return !reg.exec($input.value)
-        ? el.nextElementSibling.classList.add("message__error--show")
-        : el.nextElementSibling.classList.remove("message__error--show");
+      return !reg.exec($input.value) ?
+        el.nextElementSibling.classList.add("message__error--show") :
+        el.nextElementSibling.classList.remove("message__error--show");
     }
   });
 });
@@ -32,37 +32,25 @@ document.addEventListener("submit", (e) => {
   message__button.style.opacity = "0";
   message__loader.style.opacity = "1";
 
-  setTimeout(() => {
-    message__loader.style.opacity = "0";
-    message__response.style.opacity = "1";
-    sendMail(
-      elements["email"].value,
-      elements["message"].value,
-      elements["name"].value,
-      e.target
-    );
-  }, 1000);
+
+  fetch("https://formsubmit.co/ajax/pereshol13@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target)
+    }).then(res => { res.ok ? res.json() : Promise.reject(res) })
+    .then(json => {
+      message__loader.style.opacity = "0";
+      message__response.style.opacity = "1";
+      form.reset();
+      setTimeout(() => {
+        message__response.style.opacity = "0";
+        message__button.style.opacity = "1";
+      }, 2000)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 });
 
-const sendMail = (email, message, name, form) => {
-  Email.send({
-    Host: "smtp.mailtrap.io",
-    Username: "2cacf3ff5d0a55",
-    Password: "5af656d45c8678",
-    To: "pereshol13@gmail.com",
-    From: email,
-    Subject: "Propuesta de trabajo",
-    Body:
-      "<p><strong>Mensaje:</strong></p><br>" +
-      message +
-      "<br><br><p><strong>Nombre:</strong></p>" +
-      name,
-  }).then(() => {
-    message__response.style.opacity = "0";
-    message__button.style.opacity = "1";
-    form.reset();
-  });
-};
 if (value === "true") {
   check_dark.setAttribute("checked", "checked");
 }
@@ -83,12 +71,12 @@ nav_button.addEventListener(
 );
 
 check.addEventListener("click", () => {
-  check.checked
-    ? (location.href = "../en/")
-    : (location.href = "../");
+  check.checked ?
+    (location.href = "../en/") :
+    (location.href = "../");
 });
 
 
 check_dark.addEventListener("click", () => {
-      localStorage.setItem("check", check_dark.checked)
+  localStorage.setItem("check", check_dark.checked)
 });
